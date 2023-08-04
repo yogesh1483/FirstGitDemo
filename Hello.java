9225fdfaf70f6b2fc71982f214292c28facfa949
yogesh1483 <60321944+yogesh1483@users.noreply.github.com>
2023-08-04 11:46:07 +0530

    Update by Dev 2 Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,7 @@
 public class Hello{
 	public static void main(String[] args){
 		System.out.println("This is 1st git Program by Dev 1");
+		System.out.println("This is 1st git Program by Dev 2");
 	}
 
 }
